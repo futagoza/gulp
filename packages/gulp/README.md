868fbe7e886b231380de0abd87eed4e9fcdf185c
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2018-09-20 16:55:23 +0100

    Use @futagoza/node-run

--- packages/gulp/README.md
+++ packages/gulp/README.md
@@ -10,9 +10,7 @@ Will bump the `version` field of any file passed to it from Gulp.
 
 Will run `child_process.spawn()` wrapped in a `Promise` that is only resolved when the spawned process closes. All output by default is sent to the current process via `options.stdio = "inherit"`.
 
-The arguments are the same as `child_process.spawn()`, but if the second argument isn't an array, it will try `options.args` or `options.argv`; failing that, the first argument (the string) will be split (by default " " is used, but that can be changed using `options.ws`), and all but the first element will be used as the `args` argument, setting the first element as the new command.
-
-> Technically, this is not a Gulp plugin, but a normal Node.js function, therefore can be used without Gulp.
+> See [@futagoza/node-run](https://www.npmjs.com/package/@futagoza/node-run) for more information.
 
 ## @futagoza/gulp/publish
 
--- packages/gulp/exec.js
+++ packages/gulp/exec.js
@@ -1,45 +1,5 @@
 "use strict";
 
-const cp = require( "child_process" );
+const run = require( "@futagoza/node-run" );
 
-/**
- * Will run `child_process.spawn()` wrapped in a `Promise`.
- *
- * @param {String} command Path of the executable to run as a child process.
- * @param {String[]} [args] Arguments to pass to the child process.
- * @param {{}} [options] Options passed to `child_process.spawn()`.
- */
-function exec( command, args, options = {} ) {
-
-    if ( ! Array.isArray( args ) ) {
-
-        options = args || {};
-        args = options.args || options.argv || null;
-
-    }
-
-    if ( args === null ) {
-
-        const [ head, ...tail ] = command.split( options.ws || " " );
-
-        command = head;
-        args = tail;
-
-    }
-
-    if ( ! options.stdio ) options.stdio = "inherit";
-
-    return new Promise( ( resolve, reject ) => {
-
-        cp.spawn( command, args, options )
-
-            .on( "error", reject )
-            .on( "close", ( code, signal ) => resolve( { code, signal } ) );
-
-    } );
-
-}
-
-module.exports = exec;
-module.exports.exec = exec;
-module.exports.default = exec;
+module.exports = run;
