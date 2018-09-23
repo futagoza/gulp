768dbfaa26533ce4410db62fd5947ece10c4f339
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2018-09-23 10:58:16 +0100

    eslint -> gulp-eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,23 @@
 
 const gulp = require( "gulp" );
 const bump = require( "@futagoza/gulp/bump" );
+const eslint = require( "gulp-eslint" );
 const publish = require( "@futagoza/gulp/publish" );
 const pump = require( "pump" );
+
+// Run ESLint on all JavaScript files.
+gulp.task( "lint", () => pump(
+
+    gulp.src( [
+        "packages/**/*.js",
+        ".eslintrc.js",
+        "gulpfile.js",
+    ] ),
+    eslint( { dotfiles: true } ),
+    eslint.format(),
+    eslint.failAfterError()
+
+) );
 
 // Bump the "version" field of every `package.json` in the packages directory
 gulp.task( "bump", () => pump(
