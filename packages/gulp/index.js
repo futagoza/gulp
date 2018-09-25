b38ed90a282f4404c8a17f044e21bd2068bc110b
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2018-09-26 00:56:48 +0100

    Export @futagoza/cli-utils

--- packages/gulp/index.js
+++ packages/gulp/index.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const { color, log } = require( "@futagoza/cli-utils" );
+
 exports.bump = require( "./bump" );
+exports.color = color;
 exports.exec = require( "./exec" );
+exports.log = log;
 exports.publish = require( "./publish" );
